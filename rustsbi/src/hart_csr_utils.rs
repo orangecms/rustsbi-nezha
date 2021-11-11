4a7a800847d05c3ae0a4ba8d3744f5ef9999eaf7
Daniel Maslowski <info@orangecms.org>
2021-11-12 00:42:16 +0100

    reorder

--- rustsbi/src/hart_csr_utils.rs
+++ rustsbi/src/hart_csr_utils.rs
@@ -6,6 +6,7 @@ use riscv::register::{
     pmpaddr3, pmpaddr4, pmpaddr5, pmpaddr6, pmpaddr7, pmpaddr8, pmpaddr9, pmpcfg0, pmpcfg2,
 };
 use rustsbi::{print, println};
+
 pub const PMP_COUNT: usize = 16;
 pub const PMP_SHIFT: usize = 2;
 pub const PMP_R: usize = 0x01;
@@ -31,6 +32,108 @@ fn ctz(mut x: usize) -> usize {
     }
     ret
 }
+
+#[inline]
+fn print_misa() {
+    let isa = misa::read();
+    if let Some(isa) = isa {
+        let mxl_str = match isa.mxl() {
+            MXL::XLEN32 => "RV32",
+            MXL::XLEN64 => "RV64",
+            MXL::XLEN128 => "RV128",
+        };
+        print!("[rustsbi] misa: {}", mxl_str);
+        for ext in 'A'..='Z' {
+            if isa.has_extension(ext) {
+                print!("{}", ext);
+            }
+        }
+        println!("\r");
+    }
+}
+
+#[inline]
+fn print_mideleg() {
+    let mideleg = mideleg::read();
+    let mut delegs = Vec::new();
+    if mideleg.usoft() {
+        delegs.push("usoft")
+    }
+    if mideleg.utimer() {
+        delegs.push("utimer")
+    }
+    if mideleg.uext() {
+        delegs.push("uext")
+    }
+    if mideleg.ssoft() {
+        delegs.push("ssoft")
+    }
+    if mideleg.stimer() {
+        delegs.push("stimer")
+    }
+    if mideleg.sext() {
+        delegs.push("sext")
+    }
+    println!(
+        "[rustsbi] mideleg: {} ({:#x})\r",
+        delegs.join(", "),
+        mideleg.bits()
+    );
+}
+
+#[inline]
+fn print_medeleg() {
+    let medeleg = medeleg::read();
+    let mut delegs = Vec::new();
+    if medeleg.instruction_misaligned() {
+        delegs.push("ima")
+    }
+    if medeleg.instruction_fault() {
+        delegs.push("ia") // instruction access
+    }
+    if medeleg.illegal_instruction() {
+        delegs.push("illinsn")
+    }
+    if medeleg.breakpoint() {
+        delegs.push("bkpt")
+    }
+    if medeleg.load_misaligned() {
+        delegs.push("lma")
+    }
+    if medeleg.load_fault() {
+        delegs.push("la") // load access
+    }
+    if medeleg.store_misaligned() {
+        delegs.push("sma")
+    }
+    if medeleg.store_fault() {
+        delegs.push("sa") // store access
+    }
+    if medeleg.user_env_call() {
+        delegs.push("uecall")
+    }
+    if medeleg.supervisor_env_call() {
+        delegs.push("secall")
+    }
+    if medeleg.machine_env_call() {
+        delegs.push("mecall")
+    }
+    if medeleg.instruction_page_fault() {
+        delegs.push("ipage")
+    }
+    if medeleg.load_page_fault() {
+        delegs.push("lpage")
+    }
+    if medeleg.store_page_fault() {
+        delegs.push("spage")
+    }
+    println!(
+        "[rustsbi] medeleg: {} ({:#x})\r",
+        delegs.join(", "),
+        medeleg.bits()
+    );
+}
+
 fn pmp_get(n: usize) -> Option<(usize, usize, usize)> {
     if n >= PMP_COUNT {
         return None;
@@ -139,105 +242,4 @@ pub fn print_hart_pmp() {
             print!(")\r\n")
         }
     }
-}
-
-#[inline]
-fn print_misa() {
-    let isa = misa::read();
-    if let Some(isa) = isa {
-        let mxl_str = match isa.mxl() {
-            MXL::XLEN32 => "RV32",
-            MXL::XLEN64 => "RV64",
-            MXL::XLEN128 => "RV128",
-        };
-        print!("[rustsbi] misa: {}", mxl_str);
-        for ext in 'A'..='Z' {
-            if isa.has_extension(ext) {
-                print!("{}", ext);
-            }
-        }
-        println!("\r");
-    }
-}
-
-#[inline]
-fn print_mideleg() {
-    let mideleg = mideleg::read();
-    let mut delegs = Vec::new();
-    if mideleg.usoft() {
-        delegs.push("usoft")
-    }
-    if mideleg.utimer() {
-        delegs.push("utimer")
-    }
-    if mideleg.uext() {
-        delegs.push("uext")
-    }
-    if mideleg.ssoft() {
-        delegs.push("ssoft")
-    }
-    if mideleg.stimer() {
-        delegs.push("stimer")
-    }
-    if mideleg.sext() {
-        delegs.push("sext")
-    }
-    println!(
-        "[rustsbi] mideleg: {} ({:#x})\r",
-        delegs.join(", "),
-        mideleg.bits()
-    );
-}
-
-#[inline]
-fn print_medeleg() {
-    let medeleg = medeleg::read();
-    let mut delegs = Vec::new();
-    if medeleg.instruction_misaligned() {
-        delegs.push("ima")
-    }
-    if medeleg.instruction_fault() {
-        delegs.push("ia") // instruction access
-    }
-    if medeleg.illegal_instruction() {
-        delegs.push("illinsn")
-    }
-    if medeleg.breakpoint() {
-        delegs.push("bkpt")
-    }
-    if medeleg.load_misaligned() {
-        delegs.push("lma")
-    }
-    if medeleg.load_fault() {
-        delegs.push("la") // load access
-    }
-    if medeleg.store_misaligned() {
-        delegs.push("sma")
-    }
-    if medeleg.store_fault() {
-        delegs.push("sa") // store access
-    }
-    if medeleg.user_env_call() {
-        delegs.push("uecall")
-    }
-    if medeleg.supervisor_env_call() {
-        delegs.push("secall")
-    }
-    if medeleg.machine_env_call() {
-        delegs.push("mecall")
-    }
-    if medeleg.instruction_page_fault() {
-        delegs.push("ipage")
-    }
-    if medeleg.load_page_fault() {
-        delegs.push("lpage")
-    }
-    if medeleg.store_page_fault() {
-        delegs.push("spage")
-    }
-    println!(
-        "[rustsbi] medeleg: {} ({:#x})\r",
-        delegs.join(", "),
-        medeleg.bits()
-    );
 }
