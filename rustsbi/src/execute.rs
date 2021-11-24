f2c5870335571592297aa771f0122384cbb7644f
Daniel Maslowski <info@orangecms.org>
2021-11-25 00:43:16 +0100

    more elaborate instruction page fault prints

--- rustsbi/src/execute.rs
+++ rustsbi/src/execute.rs
@@ -16,18 +16,30 @@ pub fn execute_supervisor(supervisor_mepc: usize, a0: usize, a1: usize) -> ! {
         match Pin::new(&mut rt).resume(()) {
             GeneratorState::Yielded(MachineTrap::SbiCall()) => {
                 let ctx = rt.context_mut();
+                if ctx.a7 != 0x1 {
+                    println!("[rustsbi] Who ya gonna call?\r");
+                }
                 if emulate_sbi_call(ctx) {
                     continue;
                 }
-                feature::preprocess_supervisor_external(ctx); // specific for 1.9.1; see document for details
+                // specific for 1.9.1; see document for details
+                feature::preprocess_supervisor_external(ctx);
+                if ctx.a7 != 0x1 {
+                    println!("[rustsbi] {:x?} {:x?}\r", ctx.a7, ctx.a6);
+                    println!("{:#04X?}\r", [ctx.a0, ctx.a1, ctx.a2]);
+                }
                 let param = [ctx.a0, ctx.a1, ctx.a2, ctx.a3, ctx.a4];
                 let ans = rustsbi::ecall(ctx.a7, ctx.a6, param);
                 ctx.a0 = ans.error;
                 ctx.a1 = ans.value;
                 ctx.mepc = ctx.mepc.wrapping_add(4);
+                if ctx.a7 != 0x1 {
+                    println!("[rustsbi] {:x?} {:x?}\r", ctx.a0, ctx.a1);
+                }
             }
             GeneratorState::Yielded(MachineTrap::IllegalInstruction()) => {
                 let ctx = rt.context_mut();
+                println!("[rustsbi] Na na na! {:x?} {:x?}\r", ctx.a0, ctx.a1);
                 // FIXME: get_vaddr_u32这个过程可能出错。
                 let ins = unsafe { get_vaddr_u32(ctx.mepc) } as usize;
                 if !emulate_illegal_instruction(ctx, ins) {
@@ -45,6 +57,7 @@ pub fn execute_supervisor(supervisor_mepc: usize, a0: usize, a1: usize) -> ! {
             }
             GeneratorState::Yielded(MachineTrap::ExternalInterrupt()) => unsafe {
                 let ctx = rt.context_mut();
+                println!("[rustsbi] No no no! {:x?} {:x?}\r", ctx.a0, ctx.a1);
                 feature::call_supervisor_interrupt(ctx)
             },
             GeneratorState::Yielded(MachineTrap::MachineTimer()) => {
@@ -56,6 +69,7 @@ pub fn execute_supervisor(supervisor_mepc: usize, a0: usize, a1: usize) -> ! {
             // todo：编写样例，验证store page fault和instruction page fault
             GeneratorState::Yielded(MachineTrap::InstructionFault(addr)) => {
                 let ctx = rt.context_mut();
+                println!("[rustsbi] Na na na! {:x?} {:x?}\r", ctx.a0, ctx.a1);
                 if feature::is_page_fault(addr) {
                     unsafe {
                         feature::do_transfer_trap(
@@ -71,15 +85,17 @@ pub fn execute_supervisor(supervisor_mepc: usize, a0: usize, a1: usize) -> ! {
             }
             GeneratorState::Yielded(MachineTrap::LoadFault(_addr)) => {
                 let ctx = rt.context_mut();
-
+                println!("[rustsbi] Na na na! {:x?} {:x?}\r", ctx.a0, ctx.a1);
                 unsafe { feature::do_transfer_trap(ctx, Trap::Exception(Exception::LoadFault)) }
             }
             GeneratorState::Yielded(MachineTrap::LoadPageFault(_addr)) => {
                 let ctx = rt.context_mut();
+                println!("[rustsbi] LoadPageFault {:#04X?}\r", ctx);
                 unsafe { feature::do_transfer_trap(ctx, Trap::Exception(Exception::LoadPageFault)) }
             }
             GeneratorState::Yielded(MachineTrap::StorePageFault(addr)) => {
                 let ctx = rt.context_mut();
+                println!("[rustsbi] StorePageFault {:#04X?}\r", ctx);
                 if feature::is_page_fault(addr) {
                     unsafe {
                         feature::do_transfer_trap(ctx, Trap::Exception(Exception::LoadPageFault))
@@ -90,6 +106,7 @@ pub fn execute_supervisor(supervisor_mepc: usize, a0: usize, a1: usize) -> ! {
             }
             GeneratorState::Yielded(MachineTrap::StoreFault(addr)) => {
                 let ctx = rt.context_mut();
+                println!("[rustsbi] No no no! {:x?} {:x?}\r", ctx.a0, ctx.a1);
                 if feature::is_page_fault(addr) {
                     unsafe {
                         feature::do_transfer_trap(ctx, Trap::Exception(Exception::StorePageFault))
@@ -102,14 +119,32 @@ pub fn execute_supervisor(supervisor_mepc: usize, a0: usize, a1: usize) -> ! {
             }
             GeneratorState::Yielded(MachineTrap::InstructionPageFault(addr)) => {
                 let ctx = rt.context_mut();
-                println!("[rustsbi] {:?}", Exception::InstructionPageFault);
+                println!("\r\n[rustsbi] {:?}", Exception::InstructionPageFault);
                 println!(
                     "[rustsbi] addr: [0x{:x}] mepc: [0x{:x}] 0x{:x}",
                     addr,
                     ctx.mepc,
                     unsafe { read_reg::<usize>(addr, 0) }
                 );
-                loop {}
+                let mut a0: u32;
+                let mut a1: u32;
+                let mut a2: u32;
+                let mut t0: u32;
+                let mut t1: u32;
+                let mut t2: u32;
+                unsafe {
+                    asm!("
+                    mv  {0}, a0
+                    mv  {1}, a1
+                    mv  {2}, a2
+                    mv  {3}, t0
+                    mv  {4}, t1
+                    mv  {5}, t2
+                    ", out(reg) a0, out(reg) a1, out(reg) a2, out(reg) t0, out(reg) t1, out(reg) t2);
+                }
+                println!("[rustsbi] a0: 0x{:x}, a1: 0x{:x}, a2: 0x{:x}", a0, a1, a2);
+                println!("[rustsbi] t0: 0x{:x}, t1: 0x{:x}, t2: 0x{:x}", t0, t1, t2);
+                unsafe { asm!("wfi") }
             }
             GeneratorState::Complete(()) => unreachable!(),
         }
