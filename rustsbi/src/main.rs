6557e17dfb2f9c562f7d19e61af60bebc2ac4f49
Daniel Maslowski <info@orangecms.org>
2021-11-29 12:14:02 +0100

    rework PMP setup
    Document als the stock Nezha OpenSBI setup for reference.

--- rustsbi/src/main.rs
+++ rustsbi/src/main.rs
@@ -80,16 +80,23 @@ fn init_bss() {
     }
 }
 
+/**
+ * from OpenSBI:
+ * PMP0    : 0x0000000040000000-0x000000004001ffff (A)
+ * PMP1    : 0x0000000040000000-0x000000007fffffff (A,R,W,X)
+ * PMP2    : 0x0000000000000000-0x0000000007ffffff (A,R,W)
+ * PMP3    : 0x0000000009000000-0x000000000901ffff (
+ */
 fn init_pmp() {
     use riscv::register::*;
-    let cfg = 0b000011110000111100001111usize;
-    pmpcfg0::write(0);
-    pmpcfg2::write(0);
+    let cfg = 0x0f0f0f0f0fusize;
     pmpcfg0::write(cfg);
+    // pmpcfg2::write(0);
     pmpaddr0::write(0x40000000usize >> 2);
     pmpaddr1::write(0x40200000usize >> 2);
     pmpaddr2::write(0x80000000usize >> 2);
     pmpaddr3::write(0xc0000000usize >> 2);
+    pmpaddr4::write(0xffffffffusize >> 2);
 }
 
 fn init_plic() {
